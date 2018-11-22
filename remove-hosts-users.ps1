bc3da5725bbee0a72fa828a9ea0b85f8618d4a14
roselroa <rosel@cascadeo.com>
2018-11-22 16:42:37 +0900

    initial test

---
+++ remove-hosts-users.ps1
@@ -0,0 +1,27 @@
+Import-Module ActiveDirectory
+$DNSServer = "dc1.example.com"
+$ZoneName = "example.com"
+$FileHosts = "hosts.txt"
+ForEach ($Computer in (Get-Content $FileHosts))
+{
+  Try {
+    Remove-ADComputer $Computer -ErrorAction Stop
+    Add-Content hosts-removed.log -Value "$Computer removed"
+  }
+  Catch {
+    Add-Content hosts-removed.log -Value "$Computer not found because $($Error[0])"
+  }
+  $NodeDNS = $null
+  $NodeDNS = Get-DnsServerResourceRecord -ZoneName $ZoneName -ComputerName $DNSServer -Node $Computer -RRType A -ErrorAction SilentlyContinue
+  if($NodeDNS -eq $null){
+    Write-Host "No DNS record found"
+  } else {
+    Remove-DnsServerResourceRecord -ZoneName $ZoneName -ComputerName $DNSServer -InputObject $NodeDNS -Force
+  }
+}
+
+$FileUsers = "users.txt"
+ForEach ($User in (Get-Content $FileUsers))
+{
+  Remove-ADUser -Identity $User
+}
